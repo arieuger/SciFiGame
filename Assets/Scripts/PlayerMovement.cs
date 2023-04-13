f611d7d7d30c36d22256dca171a550840998f27c
A. Roig <antiaroig@gmail.com>
2023-04-14 01:22:09 +0200

    Arreglos animación, refactorización

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -30,37 +30,29 @@ public class PlayerMovement : MonoBehaviour {
     void Update() {
         isRunning = Input.GetKey(KeyCode.LeftShift);
         horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed * (isRunning ? 1.5f : 1f);
-        
-        animator.SetFloat("horizontalMovement", Mathf.Abs(horizontalMovement));
-        animator.SetFloat("verticalMovement", isGrounded ? 0 : rb.velocity.y);
-        animator.SetBool("isRunning", isRunning && horizontalMovement != 0);
+        if (Input.GetButtonDown("Jump")) jump = true;
 
-        if (Input.GetButtonDown("Jump")) {
-            jump = true;
-        }
+        UpdateAnimations();
     }
 
     private void FixedUpdate() {
-
         isGrounded = Physics2D.OverlapBox(groundController.position, dimensionBox, 0f, groundLayers);
-        animator.SetBool("isGrounded", isGrounded);
-
-        Move(horizontalMovement * Time.fixedDeltaTime, jump);    
-
-        jump = false;
+        Move(horizontalMovement * Time.fixedDeltaTime);     
     }
 
-    private void Move(float moving, bool jumping) {
+    private void Move(float moving) {
         Vector3 targetVelocity = new Vector2(moving, rb.velocity.y);
         rb.velocity = smoothActivated ? Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmooth) : targetVelocity;
 
         if (moving > 0 && !lookingRight) Turn();
         else if (moving < 0 && lookingRight) Turn();
 
-        if (isGrounded && jumping) {
+        if (isGrounded && jump) {
             isGrounded = false;
             rb.AddForce(new Vector2(0f, jumpForce));
         }
+
+        jump = false;
     }
 
     private void Turn() {
@@ -68,6 +60,13 @@ public class PlayerMovement : MonoBehaviour {
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
+    }
+
+    private void UpdateAnimations() {
+        animator.SetFloat("horizontalMovement", Mathf.Abs(horizontalMovement));
+        animator.SetFloat("verticalMovement", isGrounded ? 0 : rb.velocity.y);
+        animator.SetBool("isRunning", isRunning && horizontalMovement != 0);
+        animator.SetBool("isGrounded", isGrounded);
     }
 
     private void OnDrawGizmos() {

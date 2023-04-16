cccb593f82c9bc54d1ebb98f3b0cb5da5861401e
A. Roig <antiaroig@gmail.com>
2023-04-16 20:47:07 +0200

    Outline para invisibilidade, refactorización

--- Assets/Scripts/Dissolve.cs
+++ Assets/Scripts/Dissolve.cs
@@ -8,6 +8,7 @@ public class Dissolve : MonoBehaviour {
     private bool isDissolving = false;
     private float fade = 1f;
     private bool isInvisible = false;
+    private bool shouldShowOutline = false;
 
     void Start() {
         material = GetComponent<SpriteRenderer>().material;
@@ -23,13 +24,16 @@ public class Dissolve : MonoBehaviour {
         if (!isDissolving) return;
 
         if (!isInvisible) {
+            // TODO: Corrutina para slide
             fade -= Time.deltaTime;
             if (fade <= 0f) {
+                shouldShowOutline = true;
                 fade = 0f;
                 isDissolving = false;
             }
 
         } else {
+            shouldShowOutline = false;
             fade += Time.deltaTime;
             if (fade >= 1f) {
                 fade = 1f;
@@ -38,6 +42,7 @@ public class Dissolve : MonoBehaviour {
         }
 
         material.SetFloat("_DissolveAmount", fade);
+        material.SetInteger("_ShouldShowOutline", shouldShowOutline ? 1 : 0);
         
     }
 }

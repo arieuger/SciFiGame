72620f85cd70ad477c6534fa470e869b4cf7b414
A. Roig <antiaroig@gmail.com>
2023-04-16 13:39:53 +0200

    Player desaparece/reaparece presionando S

--- Assets/Scripts/Dissolve.cs
+++ Assets/Scripts/Dissolve.cs
@@ -7,26 +7,37 @@ public class Dissolve : MonoBehaviour {
     private Material material;
     private bool isDissolving = false;
     private float fade = 1f;
+    private bool isInvisible = false;
 
     void Start() {
         material = GetComponent<SpriteRenderer>().material;
     }
 
-    // Update is called once per frame
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.S)) isDissolving = true;
+        if (Input.GetKeyDown(KeyCode.S) && !isDissolving) {
+            isDissolving = true;
+            isInvisible = fade == 0f;
+        }
 
-        if (isDissolving) {
+        if (!isDissolving) return;
+
+        if (!isInvisible) {
             fade -= Time.deltaTime;
-
             if (fade <= 0f) {
                 fade = 0f;
                 isDissolving = false;
             }
 
-            material.SetFloat("_DissolveAmount", fade);
+        } else {
+            fade += Time.deltaTime;
+            if (fade >= 1f) {
+                fade = 1f;
+                isDissolving = false;
+            }
         }
+
+        material.SetFloat("_DissolveAmount", fade);
         
     }
 }

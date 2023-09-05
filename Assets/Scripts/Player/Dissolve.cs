0a73bb80857fe0416535801ce9a6438cf1ed9e3e
A. Roig <antiaroig@gmail.com>
2023-09-06 00:25:33 +0200

    Refactor invisible transition

--- Assets/Scripts/Player/Dissolve.cs
+++ Assets/Scripts/Player/Dissolve.cs
@@ -10,11 +10,7 @@ public class Dissolve : MonoBehaviour {
     private Material material;
     private LocalKeyword shouldShowOutlineKeyword;
     private SpriteRenderer sr;
-    private PlayerMovement pMov;
-
-    private bool isDissolving = false;
-    private float fade = 1f;
-    private bool isInvisible = false;
+    private bool isInvisible;
     private bool shouldShowOutline = false;
 
     void Start() {
@@ -26,36 +22,12 @@ public class Dissolve : MonoBehaviour {
 
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.S) && !isDissolving) {
-            isDissolving = true;
-            isInvisible = fade == 0f;
-        }
-
-        if (!isDissolving) return;
+        if (Input.GetKeyDown(KeyCode.S)) isInvisible = !isInvisible;
         
-        if (!isInvisible) {
-            fade -= Time.deltaTime;
-            PlayerMovement.Instance.IsRunning = false;
-            if (fade <= 0f) {
-                fade = 0f;
-                shouldShowOutline = true;
-                isDissolving = false;
-                invisibilityBar.SetActive(true);
-            }
-
-        } else {
-            invisibilityBar.SetActive(false);
-            shouldShowOutline = false;
-            fade += Time.deltaTime;
-            PlayerMovement.Instance.IsRunning = true;
-            if (fade >= 1f) {
-                fade = 1f;
-                isDissolving = false;
-            }
-        }
-
-        material.SetFloat("_DissolveAmount", fade);
-        material.SetKeyword(shouldShowOutlineKeyword, shouldShowOutline);
+        material.SetFloat("_DissolveAmount", isInvisible ? 0f : 1f);
+        material.SetKeyword(shouldShowOutlineKeyword, isInvisible);
+        invisibilityBar.SetActive(isInvisible);
+        PlayerMovement.Instance.IsRunning = !isInvisible;
         
     }
 
@@ -67,7 +39,7 @@ public class Dissolve : MonoBehaviour {
 
         while (true) {
             Color color = sr.color;
-            if (shouldShowOutline) {
+            if (isInvisible) {
                 lerpTimer += Time.deltaTime;
                 float lerp = Mathf.PingPong(lerpTimer, duration) / duration;
                 color.a = Mathf.Lerp(minAlpha, 1f, Mathf.SmoothStep(minAlpha, 1f, lerp));

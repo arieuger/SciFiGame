e7fdeb3f43c649ab5bfa336a9b83845caae269af
A. Roig <antiaroig@gmail.com>
2023-09-06 00:01:18 +0200

    Plataformas final

--- Assets/Scripts/Platforms/GenericPlatform.cs
+++ Assets/Scripts/Platforms/GenericPlatform.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GenericPlatform : MonoBehaviour
 {
@@ -12,19 +13,20 @@ public class GenericPlatform : MonoBehaviour
     [SerializeField] private Color opaqueColor;
 
     private SpriteRenderer sr;
+    private BoxCollider2D bc;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        bc = GetComponent<BoxCollider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ufo"))
         {
-            Debug.Log("Plataforma iluminada");
-            sr.sprite = opaquePlatform;
-            sr.color = opaqueColor;
+            StartCoroutine(TwinkleChangingPlatform(false));
+            bc.isTrigger = false;
         }
     }
 
@@ -33,15 +35,31 @@ public class GenericPlatform : MonoBehaviour
     {
         if (other.CompareTag("Ufo"))
         {
-            Debug.Log("Plataforma en sombra");
-            sr.sprite = transparentPlatform;
-            sr.color = transparentColor;
+            StartCoroutine(TwinkleChangingPlatform(true));
+            bc.isTrigger = true;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TwinkleChangingPlatform(bool toTransparent)
     {
-        
+        Color changedColor = toTransparent ? transparentColor : opaqueColor;
+        Sprite changedSprite = toTransparent ? transparentPlatform : opaquePlatform;
+        Color originalColor = toTransparent ? opaqueColor : transparentColor;
+        Sprite originalSprite = toTransparent ? opaquePlatform : transparentPlatform;
+
+        for (int i = 0; i < Random.Range(2,4); i++) {
+            sr.sprite = changedSprite;
+            sr.color = changedColor;
+            yield return new WaitForSeconds(Random.Range(0.01f, 0.1f));
+
+            sr.sprite = originalSprite;
+            sr.color = originalColor;
+            yield return new WaitForSeconds(Random.Range(0.01f, 0.2f));
+            
+            sr.sprite = changedSprite;
+            sr.color = changedColor;
+            yield return new WaitForSeconds(Random.Range(0.01f, 0.1f));
+        }
     }
+    
 }
